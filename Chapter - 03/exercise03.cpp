7ca1ec11595bca1de2844a02318f8034acee6d07
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-16 11:02:36 +0200

    exercises from chapter 3

---
+++ Chapter - 03/exercise03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    string  message("I have learned something new again!\n"),
+            prompt("Please input tow lines of text:"),
+            str1, str2, sum;
+
+    cout << message << endl;
+
+    cout << prompt << endl;
+
+    getline( cin, str1);
+    getline( cin, str2);
+
+    sum = str1 + " " + str2;
+    cout << sum << endl;
+
+    return 0;
+}
