ed3502136a2cfa548ebbae8d6962c2531820da95
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-04-22 23:49:01 +0200

    bouncing ball exercise C7E2

---
+++ Chapter-07/exercise2.cpp
@@ -0,0 +1,56 @@
+//bouncing ball
+
+#include <iostream>
+#include <string>
+using namespace std;
+#include <conio.h>
+#include "myMacros.h"
+
+#define ESC 27 // esc termina el programa
+unsigned long delay = 5000000;
+
+int main () 
+{
+    int x = 2, y = 2, dx = 1, speed = 0;
+    bool end = false;
+    string floor(80, '-'),
+            header = "****  Bouncing Ball ****",
+            commands = "[Esc] = Terminate      [+] = Speed up      [-] = Slow down";
+
+    COLOR(WHITE, BLUE);     CLS;
+    LOCATE(1,25);       cout << header;
+    LOCATE(24,1);       cout << floor;
+    LOCATE(25,10);      cout << commands;
+
+    while (!end)
+    {
+        LOCATE(y,x);    cout << 'o';        //bola
+        for (long wait = 0; wait < delay; ++wait)
+            ;
+        if (x == 1 || x == 79) dx = -dx; //Cambio de direccion, rebote
+        if (y == 23)
+        {
+            speed = - speed;
+            if (speed == 0) speed = -7; //"patada"
+        }
+        speed += 1;
+
+        LOCATE(y,x);    cout << ' ';    //"borra" bola
+        y += speed; x += dx;            //cambio de posicion
+
+        if (kbhit() != 0)               // Presion de tecla
+        {
+            switch(getch())             // si hay presion
+            {
+                case '+':   delay -= delay/5;   //aumenta vel (disminuye latencia!)
+                            break;
+                case '-':   delay += delay/5;   //reduce vel (aumenta latencia!)
+                            break;
+                case ESC:   end = true;         //termina
+            }                
+        }   
+    }
+    NORMAL; CLS;
+    return 0;  
+}
+
