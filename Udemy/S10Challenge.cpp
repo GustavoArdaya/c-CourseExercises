95fdd8d97a876760c23535ca5f4bf209424dab26
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-08 00:38:43 +0200

    S10 Challenge fix to support other characters

--- Udemy/S10Challenge.cpp
+++ Udemy/S10Challenge.cpp
@@ -17,10 +17,21 @@ int main () {
     cout << "Please enter message for encryption: ";
     getline(cin, original_message);
 
-    for (size_t i = 0; i < original_message.length(); i++) {
-        int index {};
-        index = alphabet.find(original_message.at(i));
-        encoded_message += key.at(index);
+    // for (size_t i = 0; i < original_message.length(); i++) {
+    //     int index {};
+    //     index = alphabet.find(original_message.at(i));        
+    //     encoded_message += key.at(index);
+    // }
+
+    for (char c : original_message) {
+        size_t position = alphabet.find(c);
+        if (position != string::npos) {
+            char new_char {key.at(position)};
+            encoded_message += new_char;
+        } else {
+            encoded_message += c;
+        }
+
     }
     cout << "\nYour encrypted message is: " << encoded_message << endl;
 
@@ -31,10 +42,19 @@ int main () {
         if (confirmation == 'y' || confirmation == 'Y') {
             cout << "y selected";
             
-            for (size_t i = 0; i < encoded_message.length(); i++) {
-                int index {};
-                index = key.find(encoded_message.at(i));
-                decoded_message += alphabet.at(index);                
+            // for (size_t i = 0; i < encoded_message.length(); i++) {
+            //     int index {};
+            //     index = key.find(encoded_message.at(i));
+            //     decoded_message += alphabet.at(index);                
+            // }
+            for (char c : encoded_message) {
+                size_t position = key.find(c);
+                if (position != string::npos) {
+                    char new_char {alphabet.at(position)};
+                    decoded_message += new_char;
+                } else {
+                    decoded_message += c;
+                }
             }
             cout << "\nYour decrypted message is: " << decoded_message << endl;
             
